c73fc46d10092e4e6d06183b7459b5bece5ec438
8enrich <henriqueftcunha@gmail.com>
2026-04-29 13:42:31 -0300

    (dev): Cors

--- src/main/java/com/karaoke/config/WebSocketConfig.java
+++ src/main/java/com/karaoke/config/WebSocketConfig.java
@@ -19,7 +19,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("*")
+                .setAllowedOriginPatterns("http://localhost:3000")
                 .withSockJS();
     }
 }
