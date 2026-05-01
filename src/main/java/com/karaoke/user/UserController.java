c70e6983c9e7de257b91428430a6e1c18c3988cd
dabzr <dylanab@pm.me>
2026-05-01 10:57:57 -0300

    Corrigindo bugs, corrigindo adição de música e adicionando endpoint que dá todos os usuários de uma dada sala

--- src/main/java/com/karaoke/user/UserController.java
+++ src/main/java/com/karaoke/user/UserController.java
@@ -2,21 +2,29 @@ package com.karaoke.user;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/room/{roomId}/{userId}")
+@RequestMapping("/room/{roomId}")
 public class UserController {
     private final UserService service;
 
     public UserController(UserService service) {
         this.service = service;
     }
+
     @GetMapping
+    public List<UserResponse> getAll(@PathVariable String roomId) {
+        return service.getAll(roomId);
+    }
+
+    @GetMapping("/{userId}")
     public User getUser(@PathVariable String roomId,
                         @PathVariable String userId) {
         return service.get(roomId, userId);
     }
 
-    @DeleteMapping
+    @DeleteMapping("/{userId}")
     public void kickUser(@PathVariable String roomId, @PathVariable String userId) {
         service.kickUser(roomId, userId);
     }
