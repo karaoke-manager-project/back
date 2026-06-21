fb7384a9e6bf5fdd90b100befe74c0ad0ea146db
dabzr <dylanab@pm.me>
2026-06-21 12:08:56 -0300

    Correção de configuração

--- src/main/java/com/karaoke/config/CorsConfig.java
+++ src/main/java/com/karaoke/config/CorsConfig.java
@@ -3,15 +3,19 @@ package com.karaoke.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.beans.factory.annotation.Value;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedMethods("*")
-                .allowedHeaders("*")
-                .allowCredentials(true);
-    }
+  @Value("${FRONT_URL}")
+  private String frontUrl;
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+        .allowedOrigins(frontUrl)
+        .allowedMethods("*")
+        .allowedHeaders("*")
+        .allowCredentials(true);
+  }
 }
